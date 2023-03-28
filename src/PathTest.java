8616f2646cbc3bba70105ce7b7f20dd89cabd000
HalfEnif <halfenif@gmail.com>
2023-03-28 21:30:23 +0900

    PathTest 변경

--- src/PathTest.java
+++ src/PathTest.java
@@ -1,26 +1,41 @@
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.nio.file.*;
 
 public class PathTest {
     public static void main(String[] args) {
-        Path base = Paths.get("C:\\temp");
+        Path base = Paths.get("/temp");
 
-        Path source = base.resolve("source");
-        source = source.resolve("새 폴더");
+        Path source = Paths.get(base.toString(), "source/새 폴더");
         source = source.resolve("소스.txt");
 
-        Path target = base.resolve("target\\새 폴더\\타겟.txt");
-
-        File sourceFile = source.toFile();
-        File targetFile = target.toFile();
+        Path target = Paths.get(base.toString(),"target/딴 폴더/타겟.txt");
 
 
+        if(!source.toFile().exists()) {
+            System.out.println("소스파일이 없습니다.");
+            return;
+        }
+
+        if(!target.getParent().toFile().exists()) {
+            System.out.println("타겟 폴더가 없습니다.");
+            return;
+        }
+        
+        if(target.toFile().exists()) {
+            System.out.println("타겟에 동일 파일이 존재합니다.");
+            return;            
+        }
+
+
+
+//, StandardCopyOption.REPLACE_EXISTING
         try {
-            Files.move(source, target, StandardCopyOption.REPLACE_EXISTING);
+            Files.move(source, target);
+        } catch (NoSuchFileException e) {
+            System.out.println("시도는 했으나 파일이 없습니다.");
+        } catch (FileAlreadyExistsException e) {
+            System.out.println("대상 파일이 이미 존재합니다.");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
