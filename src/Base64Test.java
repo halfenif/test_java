4dd800c105bf83deea2ac915ff59ca432fcc02c4
HalfEnif <halfenif@gmail.com>
2023-04-08 23:06:05 +0900

    Base64Test 추가

--- src/Base64Test.java
+++ src/Base64Test.java
@@ -1,23 +1,43 @@
 import com.google.gson.Gson;
+import org.apache.commons.text.StringEscapeUtils;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Base64;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Base64Test {
     public static void main(String[] args) {
-        String strBase64 = "eyJ0aXRsZSI6Iu2DgOydtO2LgOyeheuLiOuLpC4iLCJib2R5Ijoi67O466y4IOyyqyDspITsnoXri4jri6QuXG7rs7jrrLgg65GQIOuyiOynuCDspITsnoXri4jri6QuXG7rs7jrrLgg7IS4IOuyiOynuCDspITsnoXri4jri6QuXG7rs7jrrLgg64SkIOuyiOynuCDspITsnoXri4jri6QuXG7rs7jrrLgg64uk7ISvIOuyiOynuCDspITsnoXri4jri6QuXG4iLCJhcnJheURhdGEiOlt7ImlucHV0QXJyYXlEYXRhIjoiQXJyYXkgMeuyiOyeheuLiOuLpC4ifSx7ImlucHV0QXJyYXlEYXRhIjoiQXJyYXkgMuuyiOyeheuLiOuLpC4ifSx7ImlucHV0QXJyYXlEYXRhIjoiQXJyYXkgM+uyiOyeheuLiOuLpC4ifSx7ImlucHV0QXJyYXlEYXRhIjoiQXJyYXkgNOuyiOyeheuLiOuLpC4ifSx7ImlucHV0QXJyYXlEYXRhIjoiQXJyYXkgNeuyiOyeheuLiOuLpC4ifV19";
+        System.setProperty("java.util.logging.SimpleFormatter.format","%5$s %n");
+        Logger logger = Logger.getAnonymousLogger();
 
-        byte[] byteJson = Base64.getDecoder().decode(strBase64);
+        String filename = "base64.txt";
+
+        logger.log(Level.INFO,"Filename: {0}",filename);
+        Path path = Paths.get("data",filename);
+        byte[] byteFile = null;
+        try {
+            byteFile = Files.readAllBytes(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        String strInput = new String(byteFile);
+        byte[] byteJson = Base64.getDecoder().decode(strInput);
 
         String strJson = new String(byteJson, StandardCharsets.UTF_8);
-        System.out.println(strJson);
-
+        logger.log(Level.INFO,"JSON String:\n{0}",strJson);
+        logger.log(Level.INFO,"Json Escape:\n{0}", StringEscapeUtils.escapeJson(strJson));
 
         Gson gson = new Gson();
         HashMap map = new HashMap();
         map = (HashMap)gson.fromJson(strJson,map.getClass());
-
-        System.out.println(map.toString());
+        logger.log(Level.INFO,"MAP:\n{0}",map.toString());
     }
 }
